832407d43e4c0c72a23f32460e8e9197e75590a4
Uvindya Jayasundara <118947376+Hasanduni@users.noreply.github.com>
2025-08-25 21:58:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ if uploaded_file is not None:
     with col1:
         st.markdown(
             f"""
-            <div style="background-color:#f8f9fa;padding:20px;border-radius:12px;
-                        box-shadow:0 4px 12px rgba(0,0,0,0.1);">
+           <div style="background-color:#f8f9fa; color:#000; padding:20px; border-radius:12px;
+                box-shadow:0 4px 12px rgba(0,0,0,0.1);">
                 <h3 style="margin-top:0;color:#2c3e50;">👤 Candidate #{row['Candidate_ID']}</h3>
                 <p><b>🎓 University:</b> {row['University']}</p>
                 <p><b>📘 Course:</b> {row['Course']}</p>
@@ -93,8 +93,8 @@ if uploaded_file is not None:
     with col2:
         st.markdown(
             f"""
-            <div style="background-color:#f8f9fa;padding:20px;border-radius:12px;
-                        box-shadow:0 4px 12px rgba(0,0,0,0.1);">
+            <div style="background-color:#f8f9fa; color:#000; padding:20px; border-radius:12px;
+                box-shadow:0 4px 12px rgba(0,0,0,0.1);">
                 <p><b>💼 Current Role:</b> {row['Current_Role']}</p>
                 <p><b>👨‍🎓 Previous Internships:</b> {row['Previous_Internship']}</p>
                 <p><b>⏳ Experience:</b> {row['Experience_Years']} years</p>
